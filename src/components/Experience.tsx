9c235dd6fbe347379b89f15dc5e8b9841b45f118
PortoStack <yospunyaporto@gmail.com>
2026-05-28 19:02:57 +0700

    feat(experience): initial experience

---
+++ src/components/Experience.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Experience() {
+  return (
+    <section id="experience" className="h-screen snap-start snap-always p-16">
+      Experience
+    </section>
+  );
+}
