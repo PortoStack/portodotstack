ab3688faf4ab8c4a5d9555709986aace02ffb596
PortoStack <yospunyaporto@gmail.com>
2026-05-28 19:01:50 +0700

    feat(project): inital project

---
+++ src/components/Project.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Project() {
+  return (
+    <section
+      id="project"
+      className="h-screen snap-start snap-always px-16 py-24"
+    >
+      Project
+    </section>
+  );
+}
